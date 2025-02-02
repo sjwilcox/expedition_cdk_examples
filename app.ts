3dd21ac51c422cb1927fe0596a70c43fcdd88552
sjwilcox <76595632+sjwilcox@users.noreply.github.com>
2025-02-02 11:49:38 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { AppStack } from './app-stack'; // Import the stack
+import { AppStack } from './app'; // Import the stack
 import { ACCOUNT_ID, AWS_REGION } from './constants'; // Import from constants.ts
 
 const app = new cdk.App();
